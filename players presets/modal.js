9c003bf61aba6397691dfe22dffef785dc6ee1b6
Joaopedrov1 <joaopedro8367@gmail.com>
2023-05-25 16:35:30 -0300

    projeto1

---
+++ players presets/modal.js
@@ -0,0 +1,78 @@
+function copyText() {
+
+    var copyText = document.getElementById("myInput");
+  
+  
+    copyText.select();
+  
+  
+    document.execCommand("copy");
+  
+  }
+
+  function copyText2() {
+
+    var copyText = document.getElementById("myInput2");
+  
+  
+    copyText.select();
+  
+  
+    document.execCommand("copy");
+  
+  }
+  function copyText4() {
+
+    var copyText = document.getElementById("myInput4");
+  
+  
+    copyText.select();
+  
+  
+    document.execCommand("copy");
+  
+  }
+  function copyText5() {
+
+    var copyText = document.getElementById("myInput5");
+  
+  
+    copyText.select();
+  
+  
+    document.execCommand("copy");
+  
+  }
+  function copyText6() {
+
+    var copyText = document.getElementById("myInput6");
+  
+  
+    copyText.select();
+  
+  
+    document.execCommand("copy");
+  
+  }
+  function copyText7() {
+
+    var copyText = document.getElementById("myInput7");
+  
+  
+    copyText.select();
+  
+  
+    document.execCommand("copy");
+  
+  }
+  function copyText8() {
+
+    var copyText = document.getElementById("myInput8");
+  
+  
+    copyText.select();
+  
+  
+    document.execCommand("copy");
+  
+  }

d7613623c918aed20258657e68b0fc03bc0d519f
Joaopedrov1 <joaopedro8367@gmail.com>
2023-05-25 16:47:13 -0300

    identacao

--- players presets/modal.js
+++ players presets/modal.js
@@ -1,78 +1,78 @@
 function copyText() {
 
-    var copyText = document.getElementById("myInput");
-  
-  
-    copyText.select();
-  
-  
-    document.execCommand("copy");
-  
-  }
+  var copyText = document.getElementById("myInput");
 
-  function copyText2() {
 
-    var copyText = document.getElementById("myInput2");
-  
-  
-    copyText.select();
-  
-  
-    document.execCommand("copy");
-  
-  }
-  function copyText4() {
+  copyText.select();
 
-    var copyText = document.getElementById("myInput4");
-  
-  
-    copyText.select();
-  
-  
-    document.execCommand("copy");
-  
-  }
-  function copyText5() {
 
-    var copyText = document.getElementById("myInput5");
-  
-  
-    copyText.select();
-  
-  
-    document.execCommand("copy");
-  
-  }
-  function copyText6() {
+  document.execCommand("copy");
 
-    var copyText = document.getElementById("myInput6");
-  
-  
-    copyText.select();
-  
-  
-    document.execCommand("copy");
-  
-  }
-  function copyText7() {
+}
 
-    var copyText = document.getElementById("myInput7");
-  
-  
-    copyText.select();
-  
-  
-    document.execCommand("copy");
-  
-  }
-  function copyText8() {
+function copyText2() {
 
-    var copyText = document.getElementById("myInput8");
-  
-  
-    copyText.select();
-  
-  
-    document.execCommand("copy");
-  
-  }
+  var copyText = document.getElementById("myInput2");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
+function copyText4() {
+
+  var copyText = document.getElementById("myInput4");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
+function copyText5() {
+
+  var copyText = document.getElementById("myInput5");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
+function copyText6() {
+
+  var copyText = document.getElementById("myInput6");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
+function copyText7() {
+
+  var copyText = document.getElementById("myInput7");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
+function copyText8() {
+
+  var copyText = document.getElementById("myInput8");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}

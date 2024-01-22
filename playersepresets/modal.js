be6480d84297f2738dc97822b3cd1a4446bff6a1
Joaopedrov1 <joaopedro8367@gmail.com>
2024-01-22 14:23:53 -0300

    agoravai1

---
+++ playersepresets/modal.js
@@ -0,0 +1,126 @@
+function copyText() {
+
+  var copyText = document.getElementById("myInput");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
+
+function copyText2() {
+
+  var copyText = document.getElementById("myInput2");
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
+
+
+function copyText4() {
+
+  var copyText = document.getElementById("myInput4");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
+function copyText5() {
+
+  var copyText = document.getElementById("myInput5");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
+function copyText6() {
+
+  var copyText = document.getElementById("myInput6");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
+function copyText7() {
+
+  var copyText = document.getElementById("myInput7");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
+function copyText8() {
+
+  var copyText = document.getElementById("myInput8");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
+function copyText10() {
+
+  var copyText = document.getElementById("myInput10");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
+function copyText11() {
+
+  var copyText = document.getElementById("myInput11");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
+
+
+function copyText12() {
+
+  var copyText = document.getElementById("myInput12");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
+
+function copyText9() {
+
+  var copyText = document.getElementById("myInput9");
+
+
+  copyText.select();
+
+
+  document.execCommand("copy");
+
+}
